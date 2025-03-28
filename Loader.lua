5d569e9c9b6d5511b52739dcf3b568ccdf092220
SpxzyFire <eren.bouf@gmail.com>
2025-03-27 19:50:56 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,37 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Scamming Hub - Loader", "Synapse")
+
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Main Section")
+
+Section:NewButton("Criminality [ Pre Alpha ]", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Criminality.lua"))()
+  end)
+
+Section:NewButton("Universal", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Universal.lua"))()
+  end)
+
+Section:NewButton("Legends of Speed", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Legends%20of%20Speed.lua"))()
+  end)
+
+Section:NewButton("Ninja Legends", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Ninja%20Legends.lua"))()
+  end)
+Section:NewButton("Fisch", "", function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Fisch.Lua"))()
+  end)
+Section:NewButton("Bee Swarm Simulator", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Bee%20Swarm%20Simulator.lua"))()
+  end)
+
+
+Section:NewButton("Break In Story", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Break%20in%20Story.lua"))()
+  end)
+
+Section:NewButton("Internal Executor", "", function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptDeveloppement/Scamming-Hub---Open-Source/refs/heads/main/Internal%20Executor.lua"))()
+  end)
 
